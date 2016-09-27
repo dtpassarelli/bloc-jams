af0ce9f1ea0e4756358dea2a0d511088445c1ae8
Daniel Passarelli <danielpassarelli@MacBook-Pro.local>
2016-09-26 17:26:48 -0700

    added more play button features

--- scripts/album.js
+++ scripts/album.js
@@ -52,10 +52,75 @@ var albumPicasso = {
      }
  };
 
+// create a function called findParentElement
+var findParentByClassName = function(element, targetClass) {
+    // if element is true element is equal to current parent
+    if (element) {
+        var currentParent = element.parentElement;
+        // create a while loop that will loop as long as current parent is not equal to target class and current parent class name is not equal to null
+        while (currentParent.className != targetClass && currentParent.className !== null) {
+            // once matching class is found make match equal to current parent
+            currentParent = currentParent.parentElement;
+        }
+        //return currentParent
+        return currentParent;
+    }
+};
+
+// create switch function that takes an element as an aurgument
+var getSongItem = function(element) {
+    switch (element.className) {
+            // switch through all of the classes if matches album song buttons or the icons return the findparentbyclass function
+        case 'album-song-button':
+        case 'ion-play':
+        case 'ion-pause':
+            return findParentByClassName(element, 'song-item-number');
+            // if matches album view song class return song item number
+        case 'album-view-song-item':
+            return element.querySelector('.song-item-number');
+            // if matches song item title or song item duration return find parent by element function to find album view song item 
+        case 'song-item-title':
+        case 'song-item-duration':
+            return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
+            // if matches song item number return element
+        case 'song-item-number':
+            return element;
+            
+        // default return
+        default:
+            return;
+    }  
+};
+
+ var clickHandler = function(targetElement) {
+      var songItem = getSongItem(targetElement); 
+     
+     if (currentlyPlayingSong === null) {
+         songItem.innerHTML = pauseButtonTemplate;
+         currentlyPlayingSong = songItem.getAttribute('data-song-number');
+     } else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
+         songItem.innerHTML = playButtonTemplate;
+         currentlyPlayingSong = null;
+     } else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
+         var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
+         currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
+         songItem.innerHTML = pauseButtonTemplate;
+         currentlyPlayingSong = songItem.getAttribute('data-song-number');
+     }
+
+ };
+
+    
+
+
 var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
 var songRows = document.getElementsByClassName('album-view-song-item');
  // Album button templates
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+
+ // Store state of playing songs
+ var currentlyPlayingSong = null;
  
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
@@ -65,13 +130,28 @@ var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></
                   // Only target individual song rows during event delegation
          if (event.target.parentElement.className === 'album-view-song-item') {
              event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+             // create a function that takes and event as an argument
+             var songItem = getSongItem(event.target);
+              // if  the song item data song number is not equal to the current song playing switch back to play button
+           if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
+               songItem.innerHTML = playButtonTemplate;
+           }
          }
      });
       
      for (var i = 0; i < songRows.length; i++) {
          songRows[i].addEventListener('mouseleave', function(event) {
-                // Selects first child element, which is the song-item-number element
-             this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+                          var songItem = getSongItem(event.target);
+             var songItemNumber = songItem.getAttribute('data-song-number');
+ 
+             // #2
+             if (songItemNumber !== currentlyPlayingSong) {
+                 songItem.innerHTML = songItemNumber;
+             }
+         });
+                 songRows[i].addEventListener('click', function(event) {
+             // Event handler call
+                     clickHandler(event.target);
          });
      }
  };
